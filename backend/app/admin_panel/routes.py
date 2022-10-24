b758511a4e8b4b6ac718aeecae6740ec3877c4e6
Losharik17 <dimanormanev@yandex.ru>
2022-10-24 18:34:26 +0300

    Админка начало

--- backend/app/admin_panel/routes.py
+++ backend/app/admin_panel/routes.py
@@ -1,2 +1,4 @@
+from app.admin_panel import api
+from app.admin_panel.resources import ChangeDumpStatusResource
 
-
+api.add_resource(ChangeDumpStatusResource, '/admin_panel/change_dumb_status/<int:dump_id>')
--- backend/app/main/resources.py
+++ backend/app/main/resources.py
@@ -2,6 +2,7 @@ from werkzeug.datastructures import FileStorage
 from flask_restful import Resource, reqparse
 from app.models import Dump, User
 from flask import jsonify, make_response
+from app import db
 
 
 class DumpResource(Resource):
@@ -15,8 +16,8 @@ class DumpResource(Resource):
 
     def post(self, dump_id):
         """Создаёт свалку"""
-        self.parser.add_argument('lng', type=str, default='xxx', location='args')
-        self.parser.add_argument('lat', type=str, default='yyy', location='args')
+        self.parser.add_argument('lng', type=str, location='args')
+        self.parser.add_argument('lat', type=str, location='args')
         self.parser.add_argument('description', type=str, location='args')
         self.parser.add_argument('user_id', type=int, location='args')
         self.parser.add_argument('photo', type=FileStorage, location='files')
