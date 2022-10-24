9bb0a36b4e470cbd8bb410d977f026c0f9503fa5
Essayey <47187299+Essayey@users.noreply.github.com>
2022-10-24 20:54:19 +0300

    Add logo to navbar

--- client/web/src/components/NavBar.jsx
+++ client/web/src/components/NavBar.jsx
@@ -1,17 +1,20 @@
 import { observer } from 'mobx-react-lite'
 import React, { Fragment, useContext } from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Container, Nav, Navbar, Image } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { Context } from '..'
 import { ADD_DUMP_ROUTE, ADMIN_ROUTE, LOGIN_ROUTE, MAIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts'
+const logo = require('../static/img/logo.png')
 
 const NavBar = observer(() => {
     const { user } = useContext(Context);
     return (
-        <Navbar bg="light" variant="light">
+        <Navbar bg="success" variant="light">
             <Container>
                 <Navbar.Brand>
-                    <Link to={MAIN_ROUTE} className="normalize__link">Dumps View</Link>
+                    <Link to={MAIN_ROUTE} className="normalize__link">
+                        <Image src={String(logo)} style={{ width: 32, height: 32 }} />
+                    </Link>
                 </Navbar.Brand>
                 <Nav className="ml-auto">
                     {user.isAuth ?
