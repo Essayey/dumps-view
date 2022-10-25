80035f1978244a76c476e0653cb02419794de219
Losharik17 <dimanormanev@yandex.ru>
2022-10-25 12:06:06 +0300

    Update resources.py

--- backend/app/main/resources.py
+++ backend/app/main/resources.py
@@ -5,6 +5,7 @@ from flask import jsonify, make_response
 from app import db, access_required
 from flask_cors import cross_origin
 from flask_jwt_extended import jwt_required
+# from flask import current_user
 
 
 class DumpResource(Resource):
@@ -15,20 +16,21 @@ class DumpResource(Resource):
     def get(self):
         args = self.parser.parse_args()
 
+        # print(current_user)
+
         dump_id = args['id']
         if dump := Dump.query.filter_by(id=dump_id).first():
             return jsonify(dump)
         return jsonify({'message': 'User not found'})
 
-
     def post(self):
         """Создаёт свалку"""
-        self.parser.add_argument('lng', type=str, required=True)
-        self.parser.add_argument('lat', type=str, required=True)
-        self.parser.add_argument('description', type=str)
-        self.parser.add_argument('user_id', type=int)
-        self.parser.add_argument('photo', type=FileStorage, location='files')
+        self.parser.add_argument('lng', type=str, required=True, location="form")
+        self.parser.add_argument('lat', type=str, required=True, location="form")
+        self.parser.add_argument('description', type=str, location="form")
+        # self.parser.add_argument('photo', type=FileStorage, location='files')
         args = self.parser.parse_args()
+        print(args)
 
         new_dump = Dump(longitude=args['lng'], latitude=args['lat'], description=args['description'])
 
@@ -40,8 +42,8 @@ class DumpResource(Resource):
             if file := args['photo']:
                 file.save(f'app/static/dumps/{dump.id}.jpg')
 
-            if user := User.query.filter_by(id=args['user_id']).first() and jwt_required():
-                new_dump.users.append(user)
+            # if user := User.query.filter_by(id=args['user_id']).first() and jwt_required():
+            #     new_dump.users.append(user)
 
             db.session.commit()
             return make_response(jsonify({'result': True}), 201)
